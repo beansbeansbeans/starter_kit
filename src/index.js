f2aee785a0bad562f3aae39bb24049f007535fb2
annyuan <annyuan@anns-mbp.media.mit.edu>
2016-12-21 16:05:17 -0500

    add real nodes and edges

--- src/index.js
+++ src/index.js
@@ -8,27 +8,7 @@ import renderer from './renderer'
 import { getData } from './api'
 
 const ITERATIONS_COUNT = 50
-
-let g = graph()
-
-g.addNode('hello')
-g.addNode('world')
-
-g.addLink('hello', 'world')
-
-let layout = forceLayout3d(g)
-
-for(let i=0; i<ITERATIONS_COUNT; i++) {
-  layout.step()
-}
-
-g.forEachNode(node => {
-  console.log(layout.getNodePosition(node.id))
-})
-
-g.forEachLink(link => {
-  console.log(layout.getLinkPosition(link.id))
-})
+const g = graph()
 
 class App extends Component {
   render({}) {
@@ -44,9 +24,24 @@ Promise.all(['nodes', 'edges'].map(getData))
   .then(data => {
     render(<App />, document.body);
 
+    const nodes = data[0], edges = data[1]
+
+    for(let i=0, n=nodes.length; i<n; i++) {
+      g.addNode(i, nodes[i])
+    }
+
+    for(let i=0, l=edges.length; i<l; i++) {
+      g.addLink(edges[i].source, edges[i].target)
+    }
+
+    let layout = forceLayout3d(g)
+
+    for(let i=0; i<ITERATIONS_COUNT; i++) {
+      layout.step()
+    }
+
     renderer.initialize({
       element: document.querySelector("webgl-canvas"),
-      nodes: data[0],
-      edges: data[1]
+      nodes, edges
     })
   })
