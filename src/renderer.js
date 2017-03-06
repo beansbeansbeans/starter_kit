3046d7d57ad40b71119ec90241c07594cc7a8281
Ann Yuan <annyuan@mit.edu>
2017-03-06 07:53:15 -0500

    smtg is happening

--- src/renderer.js
+++ src/renderer.js
@@ -31,31 +31,33 @@ export default {
 
     const arrowSize = opts.pxPerBlock / 3
 
-    for(let i=0; i<opts.res; i++) { // x
-      for(let j=0; j<opts.res; j++) { // y
-        arrowVertices[(i * opts.res + j) * 18] = opts.pxPerBlock * i % (opts.res * opts.pxPerBlock)
-        arrowVertices[(i * opts.res + j) * 18 + 1] = opts.pxPerBlock * j % (opts.res * opts.pxPerBlock)
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
+    for(let i=-5; i<5; i++) { // x
+      for(let j=-5; j<5; j++) { // y
+        let multiplier = ((i + 5) * opts.res + (j + 5)) * 18
 
-        arrowVertices[(i * opts.res + j) * 18 + 2] = opts.pxPerBlock * i % (opts.res * opts.pxPerBlock) + arrowSize
-        arrowVertices[(i * opts.res + j) * 18 + 2] = opts.pxPerBlock * j % (opts.res * opts.pxPerBlock) + arrowSize
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
+        arrowVertices[multiplier] = opts.pxPerBlock * i % (opts.res * opts.pxPerBlock)
+        arrowVertices[multiplier + 1] = opts.pxPerBlock * j % (opts.res * opts.pxPerBlock)
+        arrowVertices[multiplier + 2] = 0
+
+        arrowVertices[multiplier + 2] = opts.pxPerBlock * i % (opts.res * opts.pxPerBlock) + arrowSize
+        arrowVertices[multiplier + 2] = opts.pxPerBlock * j % (opts.res * opts.pxPerBlock) + arrowSize
+        arrowVertices[multiplier + 2] = 0
         
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
+        arrowVertices[multiplier + 2] = 0
+        arrowVertices[multiplier + 2] = 0
+        arrowVertices[multiplier + 2] = 0
         
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
+        arrowVertices[multiplier + 2] = 0
+        arrowVertices[multiplier + 2] = 0
+        arrowVertices[multiplier + 2] = 0
         
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
+        arrowVertices[multiplier + 2] = 0
+        arrowVertices[multiplier + 2] = 0
+        arrowVertices[multiplier + 2] = 0
         
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
-        arrowVertices[(i * opts.res + j) * 18 + 2] = 0
+        arrowVertices[multiplier + 2] = 0
+        arrowVertices[multiplier + 2] = 0
+        arrowVertices[multiplier + 2] = 0
       }
     }
 
