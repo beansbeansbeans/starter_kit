caeb0b09b4993fe8728a1fe58889c9a76a1593fc
Ann Yuan <annyuan@mit.edu>
2017-05-28 15:04:08 -0400

    proof of concept

--- shaders/particlesFrag.glsl
+++ shaders/particlesFrag.glsl
@@ -5,7 +5,10 @@ uniform sampler2D u_particles;
 uniform vec2 u_textureSize;
 
 void main() {
-  vec2 fragCoord = gl_FragCoord.xy;
+  vec2 fragCoord = vec2(gl_FragCoord.x - 1., gl_FragCoord.y);
+  if(fragCoord.x < 0.) {
+    fragCoord.x = u_textureSize.x;
+  }
 
   float current = texture2D(u_particles, fragCoord/u_textureSize).x;
 
