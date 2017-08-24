396f0b5fd18cf82171c83c9563963b6530de63f0
Ann Yuan <annyuan@mit.edu>
2017-08-24 06:10:56 -0400

    able to add args arbitrarily

--- src/index.js
+++ src/index.js
@@ -44,6 +44,16 @@ class App extends Component {
     return (
       <app>
         <div id="webgl-wrapper"></div>
+        <button style="position:absolute" onClick={() => {
+          let node = new Node('blerg', true)
+          web.add(node, web._root)
+
+          directory[node._id] = {
+            node, inWeb: true
+          }
+
+          renderer.update(web)
+        }}>add arg</button>
       </app>
     )
   }
@@ -56,108 +66,19 @@ Promise.all(Object.keys(preload).map(k => preload[k]())).then(() => {
 
   handleResize()
 
-  let toSearch = [{ node: argument[0] }]
+  let node = argument[0]
 
-  function getRandomUntraversedChild(node) {
-    let indices = [], randomChild
+  web = new AsyncTree(node.data, {
+    moralMatrices: node.moralMatrices
+  }, node._id)
+  
+  window.web = web
+  sharedState.set("web", web)
 
-    if(typeof node === 'undefined') {
-      function traverseDF(d) {
-        return d.children.reduce((acc, curr) => {
-          if(!!acc && typeof directory[acc._id] === 'undefined') {
-            return acc
-          }
-
-          return traverseDF(curr)
-        }, d)
-      }
-
-      let match = traverseDF(argument[0])
-      if(typeof directory[match._id] === 'undefined') randomChild = match
-    } else {
-      for(let i=0; i<node.children.length; i++) indices.push(i)
-      indices = shuffle(indices)
-
-      for(let i=0; i<node.children.length; i++) { // TODO: make this random
-        let child = node.children[i]
-        if(typeof directory[child._id] === 'undefined') {
-          randomChild = child
-          break
-        }
-      }      
-    }
-
-    return randomChild
-  }
-
-  function* walk(obj) {
-    let node = obj.node, parent = obj.parent, treeNode
-
-    if(typeof directory[node._id] === 'undefined') {
-      if(typeof parent === 'undefined') {
-        web = new AsyncTree(node.data, {
-          moralMatrices: node.moralMatrices
-        }, node._id)
-        
-        window.web = web
-        sharedState.set("web", web)
-
-        treeNode = web._root
-      } else {
-        treeNode = new Node(node.data, node.supports, {
-          moralMatrices: node.moralMatrices
-        }, node._id)
-
-        web.add(treeNode, parent)
-      }
-
-      directory[treeNode._id] = { 
-        node: treeNode, inWeb: true }
-
-      if(Object.keys(directory).length === 20) mediator.publish("flip")
-
-      yield treeNode
-    } else {
-      treeNode = directory[node._id].node
-    }
-
-    let randomChild = getRandomUntraversedChild(node),
-      current = node.parent,
-      parentNode = treeNode
-
-    while(current && typeof randomChild === 'undefined') {
-      randomChild = getRandomUntraversedChild(current)
-      parentNode = directory[current._id].node
-      current = current.parent
-    }
-
-    if(!randomChild) {
-      randomChild = getRandomUntraversedChild()
-      if(randomChild) {
-        parentNode = directory[randomChild.parent._id].node
-      }
-    }
-
-    if(randomChild) {
-      yield * walk({ parent: parentNode, node: randomChild })
-    }
-  }
+  directory[web._root._id] = { 
+    node: web._root, inWeb: true }
 
   renderer.initialize({ container: document.querySelector("#webgl-wrapper"), shaders })
 
-  const iterators = []
-
-  for(let i=0; i<3; i++) {
-    iterators.push(wrapIterator(walk({ node: argument[0] }), function(result) {
-      if(!result.done) {
-        mediator.subscribe("reconcileTree", iterators[i], true)
-      }
-
-      if(i === 0) {
-        renderer.update(web)
-      }
-    }))
-  }
-
-  iterators.forEach(d => d())
+  renderer.update(web)
 })
