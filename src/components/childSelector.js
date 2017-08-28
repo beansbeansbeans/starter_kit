cf136c0289e08b5685a98c2ea5bc7899c06887aa
Ann Yuan <annyuan@mit.edu>
2017-08-28 14:22:48 -0400

    arg selector

--- src/components/childSelector.js
+++ src/components/childSelector.js
@@ -2,20 +2,22 @@ import { h, render, Component } from 'preact'
 import helpers from '../helpers/helpers'
 
 export default class ChildSelector extends Component {
+  state = { selected: this.props.options[0].id }
+
   handleChange(event) {
-    this.props.select(event.target.value)
+    this.setState({ selected: event.target.value })
   }
 
-  render({ options, select }) {
-    const optionsDOM = options.map(o => {
-      return <option 
-        value={o.id}>{o.description}</option>
-    })
-
-    optionsDOM.unshift(<option>none</option>)
-
+  render({ options, select }, { selected }) {
     return (
-      <select onChange={this.handleChange.bind(this)}>{optionsDOM}</select>
+      <div>
+        <select onChange={this.handleChange.bind(this)}>{options.map(o => {
+            return <option 
+              value={o.id}>{o.description}</option>
+          })}
+        </select>
+        <button onClick={e => select(selected)}>attack</button>
+      </div>
     )
   }
 }
