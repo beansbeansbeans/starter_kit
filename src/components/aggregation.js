cf71377b4baf92cb404b2b0e6d4971500d22e854
Ann Yuan <annyuan@mit.edu>
2018-04-09 21:05:51 -0400

    create tree

--- src/components/aggregation.js
+++ src/components/aggregation.js
@@ -13,6 +13,49 @@ class Aggregation extends Component {
     bindAll(this, [])
   }
 
+  componentDidMount() {
+    let sample = this.props.data['10']
+    console.log("data length", this.props.data['10'].length)
+    console.log(sample)
+
+    function Node(obj, dimension, parent) {
+      this.obj = obj
+      this.left = null
+      this.right = null
+      this.parent = parent
+      this.dimension = dimension
+    }
+
+    let dimensions = sample[0].encoding.length
+
+    function buildTree(points, depth, parent) {
+      let dim = depth % dimensions, median, node
+
+      if(!points.length) return null
+
+      if(points.length === 1) {
+        return new Node(points[0], dim, parent)
+      }
+
+      points.sort(function(a, b) {
+        return a[dim] - b[dim]
+      })
+
+      median = Math.floor(points.length / 2)
+      node = new Node(points[median], dim, parent)
+      node.left = buildTree(points.slice(0, median), depth + 1, node)
+      node.right = buildTree(points.slice(median + 1), depth + 1, node)
+
+      return node
+    }
+
+    console.time("kdTree")
+    let kdTree = buildTree(sample, 0, null)
+    console.timeEnd("kdTree")
+
+    console.log(kdTree)
+  }
+
   render() {
     return (
       <div id="aggregation-wrapper">
