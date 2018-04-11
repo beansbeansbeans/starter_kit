8ffb6f4a91ae2b57c6216f4d7e91adace4b2743a
Ann Yuan <annyuan@mit.edu>
2018-04-11 18:23:55 -0400

    first dropdown

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
     $ npm install
     $ npm run start
+
+
+permutations implementation steps:
+
+(in config file)
+
+embeddings = {
+  'sentence': [vec],
+  ...
+}
+
+state = {
+  sets: [
+    {
+      label: '1',
+      sentence: 'I finally ate lunch'
+    },
+    {
+      label: '2',
+      sentence: 'this movie sucked'
+    }
+  ]
+}
+
+permutations can be derived from the sentence alone
+permutations: take an array of indices representing each word, at each step perform a random transformation (choose an index randomly, then move it randomly to the left or to the right)
+then undo these transformations
+
+assume for now that i have all the embeddings
+
+- dropdown: choose a sentence
+- list all the progressions of each of the three sets of permutations
+- dropdown: choose dimensionality
+- render a circle for the base sentence
--- src/components/permutations.js
+++ src/components/permutations.js
@@ -1,21 +1,73 @@
 import { h, render, Component } from 'preact'
 import helpers from '../helpers/helpers'
 const { roundDown, bindAll, removeDuplicates, wrapIterator, shuffle, subVectors, dotProduct, vectorLength, manhattanLength, permute, fractional } = helpers
+import { encodings } from '../config'
 import randomModule from '../helpers/random'
 const random = randomModule.random(42)
+
+class Dropdown extends Component {
+  render({ options, change }) {
+    return (
+      <select onChange={e => change(e.target.value)} class="options">
+        {options.map(d => {
+          return <option 
+            value={d.id}
+            selected={d.active} class="option">{d.sentence}</option>
+        })}
+      </select>
+    )
+  }
+}
 
 class Permutations extends Component {
   constructor(props) {
     super(props)
 
-    this.setState({
+    let sentences = [
+      "i'm going to give it a marginal thumbs up. i liked it just enough .",
+      "a deliciously nonsensical comedy about a city coming apart at its seams ."
+    ]
 
+    this.setState({
+      sets: sentences.map((d, i) => {
+        return {
+          sentence: d,
+          active: i === 0,
+          id: i
+        }
+      })
     })
   }
 
-  render() {
+  componentWillMount() {
+    console.log(encodings)
+
+    bindAll(this, ['changeSentence'])
+  }
+
+  changeSentence(id) {
+    this.setState({
+      sets: this.state.sets.map(d => {
+        if(d.id == id) {
+          d.active = true
+        } else {
+          d.active = false
+        }
+
+        return d
+      })
+    })
+  }
+
+  render({}, { sets }) {
+    let activeSentence = sets.find(d => d.active)
+
+    console.log(activeSentence)
+
     return (
-      <div id="permutations">lol permutations</div>
+      <div id="permutations">
+        <Dropdown change={id => this.changeSentence(id)} options={sets} />
+      </div>
     )
   }
 }
