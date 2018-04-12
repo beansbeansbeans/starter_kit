c0cbc2a3bb261eb7b48fba030b360bcc12e03cf9
Ann Yuan <annyuan@mit.edu>
2018-04-12 17:45:17 -0400

    setup

--- scripts/createDistanceMatrix.js
+++ scripts/createDistanceMatrix.js
@@ -4,18 +4,83 @@ let distanceType = 'euclidean'
 let dimensions = 100
 
 let getDistance = {
-  'euclidean': function() {
-
+  'euclidean': function(vec) {
+    let res = 0
+    for(let i=0; i<vec.length; i++) {
+      res += Math.pow(vec[i], 2)
+    }
+    return Math.sqrt(res)
   }
 }
 
-fs.readFile(`./data/encodings_pca_${dimensions}.json`, function(err, rawPCA) {
-  let pca = JSON.parse(rawPCA)
-  let distances = []
-  console.log("lol")
-  console.log(pca.length)
+function permute(a, p) {
+  var r = [];
+  for (var i = 0; i < a.length; ++i)
+    r.push(a[p[i]]);
+  for (i = 0; i < a.length; ++i)
+    a[i] = r[i];
 
-  fs.writeFile(`./data/distance_matrix_${distanceType}_${dimensions}.json`, JSON.stringify(distances), function(err) {
-    console.log("done")
+  return a
+}
+
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    // let index = Math.floor(random.nextDouble() * counter);
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+const subVectors = (a, b) => {
+  let res = []
+  for(let i=0; i<a.length; i++) {
+    res.push(a[i] - b[i])
+  }
+  return res
+}
+
+fs.readFile(`./data/encodings_pca_100.json`, function(err, rawPCA100) {
+  fs.readFile(`./data/encodings_pca_50.json`, function(err, rawPCA50) {
+    fs.readFile(`./data/encodings_pca_10.json`, function(err, rawPCA10) {
+      let pca = JSON.parse(rawPCA100)
+      let indices = []
+      for(let i=0; i<pca.length; i++) indices.push(i)
+      indices = shuffle(indices)
+
+      pca = permute(pca, indices).slice(0, 1000)
+
+      let distances = {}
+    
+      console.log("lol")
+      console.log(pca.length)
+
+      for(let i=0; i<pca.length; i++) {
+        let obj = pca[i]
+        distances[obj.id] = {}
+        for(let j=i+1; j<pca.length; j++) {
+          let target = pca[j]
+          distances[obj.id][target.id] = getDistance[distanceType](subVectors(target.encoding, obj.encoding))
+        }
+
+        if(Math.random() < 0.01) console.log(i)
+      }
+
+      fs.writeFile(`./data/distance_matrix_${distanceType}_${dimensions}.json`, JSON.stringify(distances), function(err) {
+        console.log("done")
+      })
+    })
   })
 })
--- src/components/distance_matrix.js
+++ src/components/distance_matrix.js
@@ -3,6 +3,10 @@ import helpers from '../helpers/helpers'
 const { roundDown, bindAll, removeDuplicates, wrapIterator, shuffle, subVectors, dotProduct, vectorLength, manhattanLength, permute, fractional, degreesToRadians, trim } = helpers
 import randomModule from '../helpers/random'
 const random = randomModule.random(42)
+import { getData, getShader } from '../api'
+
+let distances = ['euclidean']
+let dimensions = ['100']
 
 class Dropdown extends Component {
   render({ options, change }) {
@@ -22,20 +26,111 @@ class DistanceMatrix extends Component {
   constructor(props) {
     super(props)
 
-    this.setState({})
+    this.setState({
+      data: null,
+      max: 0,
+      dimensions: dimensions.map((d, i) => {
+        return {
+          active: i === 0,
+          label: d,
+          id: d
+        }
+      }),
+      distances: distances.map((d, i) => {
+        return {
+          active: i === 0,
+          label: d,
+          id: d
+        }
+      })
+    })
   }
 
   componentWillMount() {
-    bindAll(this, [])
+    bindAll(this, ['changeDropdown'])
+
+    let files = []
+
+    for(let i=0; i<distances.length; i++) {
+      for(let j=0; j<dimensions.length; j++) {
+        files.push(`distance_matrix_${distances[i]}_${dimensions[j]}`)
+      }
+    }
+
+    Promise.all(files.map(getData)).then(data => {
+      console.log(data)
+      let canvas = document.querySelector("#distance_matrix #canvas")
+      this.ctx = canvas.getContext('2d')
+      let keys = Object.keys(data[0])
+      let canvasSize = keys.length
+      let max = 0
+
+      canvas.width = 2 * canvasSize
+      canvas.height = 2 * canvasSize
+      canvas.style.width = canvasSize + 'px'
+      canvas.style.height = canvasSize + 'px'
+
+      this.ctx.scale(2, 2)
+
+      for(let i=0; i<keys.length; i++) {
+        let key = keys[i]
+        let targetKeys = Object.keys(data[0][key])
+        for(let j=0; j<targetKeys.length; j++) {
+          let val = data[0][key][targetKeys[j]]
+
+          if(val > max) max = val
+        }
+      }
+
+      this.setState({ data: data[0], max })
+    })
+  }
+
+  componentDidMount() {
+
+  }
+
+  changeDropdown(id, key) {
+    this.setState({
+      [key]: this.state[key].map(d => {
+        if(d.id == id) {
+          d.active = true
+        } else {
+          d.active = false
+        }
+
+        return d
+      })
+    })
   }
 
   componentDidUpdate() {
+    let { data, max } = this.state
+    console.log(max)
+    if(data) {
+      let keys = Object.keys(data)
+      let size = keys.length
 
+      this.ctx.clearRect(0, 0, size, size)
+
+      for(let row=0; row<size; row++) {
+        for(let col=0; col<size; col++) {
+          let val = 0.5
+          this.ctx.fillStyle = `rgba(0, 0, 0, ${1 - (val / max)})`
+          this.ctx.fillRect(col, row, 1, 1)
+        }
+      }
+    }
   }
 
-  render({}, {}) {
+  render({}, {distances, dimensions, data}) {
     return (
-      <div id="distance_matrix">distance matrix</div>
+      <div id="distance_matrix">
+        <Dropdown change={id => this.changeDropdown(id, 'dimensions')} options={dimensions} />
+        <Dropdown change={id => this.changeDropdown(id, 'distances')} options={distances} />
+        <br/>
+        <canvas id="canvas"></canvas>
+      </div>
     )
   }
 }
