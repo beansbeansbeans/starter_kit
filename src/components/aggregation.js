89c331557dbb3198edc156bba081a4845d50d48b
Ann Yuan <annyuan@mit.edu>
2018-04-13 09:11:07 -0400

    use precomputed

--- src/components/aggregation.js
+++ src/components/aggregation.js
@@ -3,6 +3,7 @@ import helpers from '../helpers/helpers'
 const { roundDown, bindAll, removeDuplicates, wrapIterator, shuffle, subVectors, dotProduct, vectorLength, manhattanLength, permute, fractional } = helpers
 import randomModule from '../helpers/random'
 const random = randomModule.random(42)
+import { getData, getShader } from '../api'
 
 const getDistance = {
   'euclidean': vectorLength,
@@ -14,12 +15,6 @@ let closestCount = 3
 const resolution = 20
 
 class Aggregation extends Component {
-  state = {
-    scale: 1,
-    scaledData: [],
-    bins: []
-  }
-
   constructor(props) {
     super(props)
 
@@ -27,127 +22,40 @@ class Aggregation extends Component {
     for(let i=0; i<resolution; i++) bins.push(0)
 
     this.setState({
-      bins: bins
+      bins: bins,
+      scale: 0,
+      data: null
     })
   }
 
   componentWillMount() {
-    bindAll(this, ['computeBins', 'aggregate'])
+    bindAll(this, ['aggregate'])
+
+    Promise.all(['aggregations'].map(getData)).then(resp => {
+      this.setState({ data: resp[0] })
+    })
   }
 
   aggregate() {
-    let { scaledData, scale } = this.state
-    let newScaledData = []
-
-    console.time("aggregate")
-    for(let i=0; i<scaledData.length; i++) {
-      let source = scaledData[i]
-      if(source.used) continue
-
-      let vec = source.encoding
-      source.used = true
-
-      let closest = []
-      closestCount = 2 + Math.floor(random.nextDouble() * 4)
-      
-      for(let j=0; j<scaledData.length; j++) {
-        let target = scaledData[j]
-        if(target.used) continue
-
-        let diff = subVectors(vec, target.encoding)
-        let distance = getDistance['euclidean'](diff)
-
-        // if it's closer than any of the elements in closer (comment out this if block for random aggregation)
-        if(closest.length === closestCount) {
-          let toDelete = -1
-          for(let k=0; k<closest.length; k++) {
-            if(distance < closest[k].distance) {
-              toDelete = k
-              break
-            }
-          }
-
-          closest.splice(toDelete, 1)
-        }
-
-        if(closest.length < closestCount) {
-          closest.push(Object.assign({ distance, index: j }, target))
-        }
-      }
-
-      for(let j=0; j<closest.length; j++) {
-        scaledData[closest[j].index].used = true
-      }
-
-      let closestLength = closest.length
-      let average = {
-        encoding: vec,
-        polarity: source.polarity
-      } 
-
-      if(closestLength > 0) {
-        average = {
-          encoding: vec.map((d, i) => {
-            return (d + closest.reduce((acc, curr) => acc + curr.encoding[i])) / (closestLength + 1)
-          }),
-          polarity: (source.polarity + closest.reduce((acc, curr) => acc + curr.polarity, 0)) / (closestLength + 1)
-        }        
-      }
-
-      newScaledData.push(average)
-    }
-    console.timeEnd("aggregate")
-    console.log(newScaledData.length)
-
-    this.setState({ scaledData: newScaledData, scale: scale + 1 }, this.computeBins)
+    this.setState({ scale: this.state.scale + 1 })
   }
 
-  computeBins() {
-    let { scaledData } = this.state
-    let bins = []
-    for(let i=0; i<resolution; i++) bins.push(0)
-
-    for(let i=0; i<scaledData.length; i++) {
-      let obj = scaledData[i]
-
-      for(let j=0; j<bins.length; j++) {
-        if(obj.polarity <= j / bins.length || j === bins.length - 1) {
-          bins[j]++
-          break
-        }
-      }
+  render({}, { bins, data, scale }) {
+    let theBins = []
+    if(data) {
+      theBins = data.euclidean['100'][scale]
     }
-
-    console.log(bins)
-
-    this.setState({ bins })
-  }
-
-  componentDidMount() {
-    let sample = this.props.data['50']
-    let scaledData = []
-
-    for(let i=0; i<sample.length; i++) {
-      scaledData.push(Object.assign(sample[i], {
-        polarity: sample[i].polarity === 'pos' ? 1 : 0
-      }))
-    }
-    
-    this.setState({ scaledData }, this.computeBins)
-  }
-
-  render({}, { bins, scale }) {
-    let total = bins.reduce((acc, curr) => acc + curr, 0)
+    let total = theBins.reduce((acc, curr) => acc + curr, 0)
 
     return (
       <div id="aggregation-wrapper">
-        <div id="histogram">{bins.map(bin => {
+        <div id="histogram">{theBins.map(bin => {
           return <div class="bin">
             <div style={`height: ${100 * bin/total}%`} class="contents"></div>
           </div>
         })}</div>
-        <div id="labels">{bins.map((bin, i) => {
-          return <div class="label">{(i / bins.length).toFixed(2)}</div>
+        <div id="labels">{theBins.map((bin, i) => {
+          return <div class="label">{(i / theBins.length).toFixed(2)}</div>
         })}</div>
         <div class="input-wrapper">
           <div class="scale">{scale}</div>
