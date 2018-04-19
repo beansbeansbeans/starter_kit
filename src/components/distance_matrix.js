bfce2ddbe08ba3fe94e6b2990022a7ef74e6be5c
Ann Yuan <annyuan@mit.edu>
2018-04-19 14:22:23 -0400

    allow highlighting of sentences

--- src/components/distance_matrix.js
+++ src/components/distance_matrix.js
@@ -14,7 +14,7 @@ let sortBy = [{
   perm: [106, 49, 206, 317, 435, 482, 104, 132, 35, 138, 400, 309, 454, 360, 448, 203, 243, 340, 22, 168, 304, 292, 201, 230, 404, 107, 428, 69, 98, 145, 487, 65, 439, 444, 33, 410, 423, 257, 190, 402, 248, 411, 28, 80, 238, 192, 490, 4, 50, 273, 296, 108, 338, 440, 182, 287, 270, 6, 253, 422, 86, 136, 152, 151, 285, 126, 127, 154, 368, 231, 381, 177, 262, 261, 446, 479, 150, 7, 23, 70, 21, 301, 302, 121, 188, 369, 216, 229, 155, 450, 432, 20, 333, 18, 205, 19, 466, 480, 160, 278, 447, 9, 259, 191, 286, 113, 161, 218, 438, 125, 228, 43, 265, 5, 354, 370, 497, 101, 47, 409, 299, 386, 84, 331, 29, 373, 0, 289, 213, 361, 387, 195, 408, 52, 268, 75, 27, 224, 143, 306, 472, 163, 399, 255, 89, 335, 314, 493, 117, 495, 46, 60, 40, 200, 274, 128, 406, 38, 10, 196, 441, 133, 220, 64, 99, 247, 343, 469, 197, 204, 443, 131, 429, 184, 456, 212, 478, 187, 193, 342, 227, 319, 134, 312, 295, 463, 232, 135, 344, 379, 164, 460, 419, 72, 103, 165, 275, 294, 122, 347, 30, 172, 221, 252, 280, 346, 42, 186, 293, 199, 311, 300, 350, 8, 374, 105, 396, 476, 118, 394, 120, 481, 24, 277, 298, 349, 356, 405, 169, 322, 45, 91, 398, 36, 166, 115, 451, 2, 383, 78, 159, 90, 291, 282, 345, 457, 353, 367, 44, 449, 34, 391, 85, 464, 170, 426, 210, 318, 194, 110, 149, 51, 416, 245, 54, 326, 235, 403, 100, 467, 183, 239, 250, 427, 485, 384, 12, 116, 222, 430, 266, 249, 87, 153, 14, 477, 421, 455, 264, 109, 305, 308, 39, 88, 320, 139, 323, 56, 233, 237, 458, 73, 208, 77, 465, 358, 141, 407, 25, 390, 434, 156, 414, 321, 41, 417, 63, 418, 462, 68, 307, 362, 483, 494, 236, 355, 258, 401, 437, 185, 48, 94, 76, 372, 114, 313, 179, 431, 453, 15, 96, 471, 144, 376, 397, 499, 129, 260, 209, 316, 415, 452, 365, 97, 496, 147, 412, 175, 366, 142, 241, 1, 251, 81, 303, 225, 325, 380, 26, 202, 352, 377, 16, 130, 174, 189, 470, 173, 180, 382, 17, 53, 389, 269, 436, 215, 31, 158, 198, 475, 137, 214, 124, 267, 254, 279, 276, 491, 244, 336, 388, 473, 359, 283, 348, 272, 123, 3, 11, 58, 271, 102, 146, 364, 375, 328, 378, 82, 119, 474, 83, 181, 157, 217, 242, 351, 226, 488, 498, 219, 140, 468, 71, 330, 57, 329, 327, 445, 281, 32, 459, 207, 288, 93, 13, 167, 176, 332, 112, 393, 489, 290, 425, 263, 148, 246, 61, 334, 66, 79, 420, 486, 62, 395, 297, 256, 337, 55, 424, 284, 461, 341, 492, 171, 95, 433, 442, 484, 234, 315, 363, 59, 178, 37, 385, 392, 324, 339, 310, 357, 67, 223, 111, 92, 162, 240, 211, 413, 74, 371]
 }]
 
-let dimensions = ['100']
+let dimensions = ['100'], sentences = []
 
 class Dropdown extends Component {
   render({ options, change }) {
@@ -49,6 +49,7 @@ class DistanceMatrix extends Component {
       canvasTop: 0,
       canvasLeft: 0,
       max,
+      highlightedSentences: [],
       dragging: false,
       dimensions: dimensions.map((d, i) => {
         return {
@@ -76,8 +77,8 @@ class DistanceMatrix extends Component {
       highlightRegion: {
         x: 0,
         y: 0,
-        width: 10,
-        height: 10
+        width: 0,
+        height: 0
       }
     })
   }
@@ -93,8 +94,11 @@ class DistanceMatrix extends Component {
       }
     }
 
+    files.push('distance_matrix_sentences')
+
     Promise.all(files.map(getData)).then(resp => {
-      console.log(resp)
+      sentences = resp[files.length - 1]
+
       let data = this.state.data
       let canvas = document.querySelector("#distance_matrix #canvas")
       this.ctx = canvas.getContext('2d')
@@ -154,7 +158,26 @@ class DistanceMatrix extends Component {
     })
 
     window.addEventListener("mouseup", e => {
-      this.setState({ dragging: false })
+      let { x, y, width, height } = this.state.highlightRegion
+
+      let indices = []
+      for(let i=0; i<width; i++) {
+        indices.push(x + i)
+      }
+
+      for(let i=0; i<height; i++) {
+        if(indices.indexOf(y + i) === -1) indices.push(y + i)
+      }
+
+      this.setState({ 
+        dragging: false,
+        highlightedSentences: indices.map(index => {
+          return {
+            index,
+            sentence: sentences[index].sentence
+          }
+        })
+      })
     })
   }
 
@@ -212,7 +235,16 @@ class DistanceMatrix extends Component {
     }, this.draw)
   }
 
-  render({}, {distances, sortBy, dimensions, data, highlightRegion, canvasSize, canvasLeft, canvasTop}) {
+  render({}, {distances, sortBy, dimensions, data, highlightRegion, canvasSize, canvasLeft, canvasTop, highlightedSentences}) {
+
+    let sentences = null
+
+    if(highlightedSentences.length) {
+      sentences = highlightedSentences.map(d => {
+        return <div class="sentence">{d.index + ': ' + d.sentence}</div>
+      })
+    }
+
     return (
       <div id="distance_matrix">
         <div>DIMENSIONS</div>
@@ -220,7 +252,18 @@ class DistanceMatrix extends Component {
         <div>COLOR BY</div>
         <Dropdown change={id => this.changeDropdown(id, 'distances')} options={distances} />
         <div>SORT BY</div>
-        <Dropdown change={id => this.changeDropdown(id, 'sortBy')} options={sortBy} />
+        <Dropdown change={id => {
+          this.changeDropdown(id, 'sortBy')
+          this.setState({
+            highlightRegion: {
+              x: 0,
+              y: 0,
+              width: 0,
+              height: 0
+            },
+            highlightedSentences: []
+          })
+        }} options={sortBy} />
         <br/>
         <div onMouseDown={e => {
           highlightRegion.x = e.clientX - canvasLeft
@@ -235,6 +278,10 @@ class DistanceMatrix extends Component {
         }} style={`width:${canvasSize}px;height:${canvasSize}px`} class="canvas_wrapper">
           <canvas id="canvas"></canvas>
           <div style={`width:${highlightRegion.width}px;height:${highlightRegion.height}px;top:${highlightRegion.y}px;left:${highlightRegion.x}px;`} class="highlight-region"></div>
+        </div>
+        <div id="highlighted_sentences">
+          <div class="label">HIGHLIGHTED SENTENCES</div>
+          {sentences}
         </div>
       </div>
     )
