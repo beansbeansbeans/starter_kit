42e4d1320b600ec1362456ac71a94197ee0896c7
Ann Yuan <annyuan@mit.edu>
2018-04-29 15:34:41 -0400

    introduction

--- src/sections/introduction.js
+++ src/sections/introduction.js
@@ -5,12 +5,22 @@ let example = {
   'sentence': "all mixed up together like a term paper from a kid who can't quite distinguish one sci-fi work from another"
 }
 
+const cellMinOpacity = 0.15
+
 class Introduction extends Component {
   componentWillMount() {
     let { data } = this.props
     console.log(data)
 
-    example.embedding = data[100].find(d => d.sentence.indexOf(example.sentence) > -1).encoding.map(d => d.toFixed(2))
+    let embedding = data[100].find(d => d.sentence.indexOf(example.sentence) > -1).encoding
+
+    example.embedding = embedding.map(d => d.toFixed(3))
+    example.embeddingSorted = embedding.sort((a, b) => a - b).reverse().map(d => d.toFixed(3))
+    example.embeddingMax = Math.max(...embedding)
+    example.embeddingMin = Math.min(...embedding)
+
+    console.log(example.embeddingMax)
+    console.log(example.embeddingMin)
   }
 
   render() {
@@ -22,7 +32,16 @@ class Introduction extends Component {
           <h4>Sentence:</h4>
           <p class="sentence">{`“${example.sentence}”`}</p>
           <h4>Embedding:</h4>
-          <div>{example.embedding.join(", ")}</div>
+          <h5>STEP 0: Raw</h5>
+          <div class="vector">{example.embedding.join(", ")}</div>
+          <h5>STEP 1: Sorted</h5>
+          <div class="vector">{example.embeddingSorted.join(", ")}</div>
+          <h5>STEP 2: Colored</h5>
+          <div class="vector">{example.embeddingSorted.map(d => <div class="cell" style={`background: rgba(123, 43, 20, ${((d - example.embeddingMin) / (example.embeddingMax - example.embeddingMin))})`}></div>)}</div>
+          <h5>STEP 3: Spiralized</h5>
+          <video width="240" height="240" autoplay loop>
+            <source src="videos/spiral_emb.mp4" type="video/mp4" />
+          </video>
         </figure>
       </div>
     )
