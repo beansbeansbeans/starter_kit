2a23315e14e57f7307e4df259643ae6571f3d602
Ann Yuan <annyuan@mit.edu>
2018-05-02 08:21:42 -0400

    some notes

--- src/sections/multiple_sentences.js
+++ src/sections/multiple_sentences.js
@@ -11,8 +11,11 @@ class MultipleSentences extends Component {
   render({ data }) {
     return (
       <div class="section-contents">
-        <p>Investigation into a corpora of sentence embeddings.</p>
+        <p>Investigating a corpora of sentence embeddings.</p>
+        <p>The point of the distance matrix visualization is to show how the clusters you'll find in your data depend greatly on the embedding model and the distance metric used. </p>
         <DistanceMatrix data={data} />
+        <p>Now that we've seen how the clusters change, it would be great to discuss the tradeoffs between model / distance metric choices.</p>
+        <p>E.g., if you use [insert model] under [insert distance metric], you'll tend to get clusters that have X quality. On the other hand, if you're looking for clusters with Y quality, you'd be better off using [insert model] under [insert distance metric].</p>
       </div>
     )
   }
